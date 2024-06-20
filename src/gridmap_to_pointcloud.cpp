710cd93dfe0e6180dcbe6f70ce89e4f11010da1a
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-06-20 23:31:24 +0900

    Fix format (#5)

--- src/gridmap_to_pointcloud.cpp
+++ src/gridmap_to_pointcloud.cpp
@@ -79,13 +79,13 @@ public:
    */
   void process(void)
   {
-    ros::Rate rate(hz_);
     const nav_msgs::OccupancyGrid gridmap = get_map();
     sensor_msgs::PointCloud2 cloud_msg;
     convert_gridmap_to_pointcloud(gridmap, cloud_msg);
     cloud_msg.header.frame_id = gridmap.header.frame_id;
     cloud_msg.header.stamp = ros::Time::now();
 
+    ros::Rate rate(hz_);
     while (ros::ok())
     {
       cloud_pub_.publish(cloud_msg);
